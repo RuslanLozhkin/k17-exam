181d5ff21a320b0662744c0f5fbc9a20116bf0fd
RuslanLozhkin <ruslanlozhkin@gmail.com>
2019-05-04 23:26:19 +0300

    almost done. need to fix details.

--- js/main.js
+++ js/main.js
@@ -8,12 +8,27 @@ $(document).ready(function(){
   arrows: false,
   customPaging: function(slider, i) {    
       return '<a href="#"><img src="img/paging.png"><img src="img/activePaging.png"></a>';
-  },
+  }
   });
+  $('.gallery').slick({
+    autoplay: false,
+    dots: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    arrows: false
+    });
   $('.prev').click(function(){
     $('.carousel').slick('slickPrev');  
   });
   $('.next').click(function(){
     $('.carousel').slick('slickNext');
+  });
+  $('.prev__gallery').click(function(){
+    $('.gallery').slick('slickPrev');  
+  });
+  $('.next__gallery').click(function(){
+    $('.gallery').slick('slickNext');
   });
 });
